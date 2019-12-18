fff2bb6e1baf076d742c4bf6575312e48985bc0d
Krisztian Papp <fejlesztes@letscode.hu>
2019-12-18 12:21:26 +0100

    webpack 6

--- webpack-basics/webpack.config.js
+++ webpack-basics/webpack.config.js
@@ -22,6 +22,15 @@ module.exports = {
                     },
                     "css-loader"
                 ]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,  
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 2000
+                    }
+                }] 
             }
         ]
     }
